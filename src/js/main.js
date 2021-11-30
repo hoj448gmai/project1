930fba3ae294df1c3d032924e70170608647f568
Samakova Irem <hoj448@gmail.com>
2021-11-30 11:00:53 +0300

    load

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+let burger = $ (".burger");
+let navigation = $ (".header__nav");
+
+
+burger.on( "click", function () {
+    burger.toggleClass("burger_active");
+    navigation.toggleClass("header__nav_active");
+
+});
+
+
